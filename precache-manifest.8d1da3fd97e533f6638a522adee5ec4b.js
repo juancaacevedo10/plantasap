f69b1e681c87e768b91f05efd73640bc74fd7b71
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-05-28 09:26:52 -0500

    Updates

---
+++ precache-manifest.8d1da3fd97e533f6638a522adee5ec4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2246ad80fe838193d6e852790a79c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24f9d5303c13f7b61a55",
+    "url": "/static/css/main.0a54a213.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f9d5303c13f7b61a55",
+    "url": "/static/js/main.dba4391d.chunk.js"
+  },
+  {
+    "revision": "b47735ed5b9c71bfcf2d",
+    "url": "/static/js/runtime-main.80f965a6.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/static/media/logo.0d3d7212.png"
+  }
+]);
